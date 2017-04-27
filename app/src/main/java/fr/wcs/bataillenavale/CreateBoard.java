8e12e9345e4a6888127bf797ab62de6b4e7839c1
apprenti <bravo.pauline@gmail.com>
2017-04-27 21:15:26 +0200

    drag sans drop

--- app/src/main/java/fr/wcs/bataillenavale/CreateBoard.java
+++ app/src/main/java/fr/wcs/bataillenavale/CreateBoard.java
@@ -1,10 +1,24 @@
 package fr.wcs.bataillenavale;
 
+import android.app.FragmentManager;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.ClipData;
+import android.graphics.Typeface;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnTouchListener;
+import android.widget.TextView;
 
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -15,17 +29,30 @@ public class CreateBoard extends AppCompatActivity implements View.OnClickListen
     TextView textViewCheckpoint;
     Button buttonReady;
     LinearLayout board;
+    ImageView vaisseau,vaisseau2, vaisseau3, vaisseau4, vaisseau5;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_board);
+
+        //views to drag
+        vaisseau = (ImageView) findViewById(R.id.vaisseau);
+        vaisseau2 = (ImageView) findViewById(R.id.vaisseau2);
+        vaisseau3 = (ImageView) findViewById(R.id.vaisseau3);
+        vaisseau4 = (ImageView) findViewById(R.id.vaisseau4);
+        vaisseau5 = (ImageView) findViewById(R.id.vaisseau5);
+
+        // view to drop
+        board = (LinearLayout) findViewById(R.id.board);
+
+
 
         textViewCheckpoint = (TextView) findViewById(R.id.textViewCheckpoint);
         buttonReady = (Button) findViewById(R.id.buttonReady);
         buttonReady.setOnClickListener(this);
 
-        board = (LinearLayout) findViewById(R.id.board);
+
 
         Intent choiceName = getIntent();
         String player1Name = choiceName.getStringExtra("player1Name");
@@ -39,6 +66,70 @@ public class CreateBoard extends AppCompatActivity implements View.OnClickListen
 
         board.addView(pixelGrid);
         board.setVisibility(View.INVISIBLE);
+
+        //set touch listeners
+        vaisseau.setOnTouchListener(new ChoiceTouchListener());
+        vaisseau2.setOnTouchListener(new ChoiceTouchListener());
+        vaisseau3.setOnTouchListener(new ChoiceTouchListener());
+        vaisseau4.setOnTouchListener(new ChoiceTouchListener());
+        vaisseau5.setOnTouchListener(new ChoiceTouchListener());
+
+        //set drag listeners
+        board.setOnDragListener(new ChoiceDragListener());
+
+
+    }
+    private final class ChoiceTouchListener implements OnTouchListener {
+
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                //setup drag
+
+                ClipData data = ClipData.newPlainText("", "");
+                DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                //start dragging the item touched
+                view.startDrag(data, shadowBuilder, view, 0);
+
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        }
+
+    }
+    private class ChoiceDragListener implements OnDragListener {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            //handle drag events
+
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DROP:
+                    //handle the dragged view being dropped over a drop view
+
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    //no action necessary
+                    break;
+                default:
+                    break;
+            }
+
+
+
+            return true;
+        }
+
     }
 
     @Override
@@ -51,42 +142,13 @@ public class CreateBoard extends AppCompatActivity implements View.OnClickListen
 
     }
 
-    private void placeBoatsView() {
-        setContentView(R.layout.activity_create_board);
-        mRetainedFragment.setCurrentView("placeBoatsView");
-        /* Define the image objects */
-        ImageView vaisseau = (ImageView) findViewById(R.id.vaisseau);
-        ImageView battleship = (ImageView) findViewById(R.id.vaisseau2);
-        ImageView destroyer = (ImageView) findViewById(R.id.vaisseau3);
-        ImageView submarine = (ImageView) findViewById(R.id.vaisseau4);
-        ImageView patrol = (ImageView) findViewById(R.id.vaisseau5);
-        game.getPlayer1Board().boardView = (BoardView) findViewById(R.id.humanBoardView);
-        game.getPlayer1Board().boardView.setBoard(game.getPlayer1Board());
 
-        // This means the user had already placed boats on the grid but decided to go back to this view and perhaps
-        // change the boats.
-        if (game.getPlayer1Board().grid != null) {
-            game.getPlayer1Board().boardView.coordinatesOfPlayer1Ships = game.getPlayer1Board().grid;
-            game.getPlayer1Board().boardView.invalidate();
-        }
 
-        /* Allow these boat images to be draggable, listen when they are touched */
-        aircraft.setOnTouchListener(new MyTouchListener());
-        battleship.setOnTouchListener(new MyTouchListener());
-        destroyer.setOnTouchListener(new MyTouchListener());
-        submarine.setOnTouchListener(new MyTouchListener());
-        patrol.setOnTouchListener(new MyTouchListener());
 
-        /* Define the particular location where these boat images are allowed to be dragged onto */
-        findViewById(R.id.humanBoardPlacer).setOnDragListener(new MyDragListener());
-        ;
-        Button next = (Button) findViewById(R.id.next); // Advance to the next view
-        Button random = (Button) findViewById(R.id.random); // Place boats at random
-        changeFont(next);
-        changeFont(random);
-        next.setVisibility(View.VISIBLE);
+
+
 
 
     }
 
-}
+
