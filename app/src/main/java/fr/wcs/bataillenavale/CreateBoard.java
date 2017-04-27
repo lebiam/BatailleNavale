5360fa5a4da55cdb5837c1f92f4c119d3e7d43b8
apprenti <bravo.pauline@gmail.com>
2017-04-27 15:50:50 +0200

    debut drag and drop

--- app/src/main/java/fr/wcs/bataillenavale/CreateBoard.java
+++ app/src/main/java/fr/wcs/bataillenavale/CreateBoard.java
@@ -40,6 +40,42 @@ public class CreateBoard extends AppCompatActivity implements View.OnClickListen
 
     }
 
+    private void placeBoatsView() {
+        setContentView(R.layout.activity_create_board);
+        mRetainedFragment.setCurrentView("placeBoatsView");
+        /* Define the image objects */
+        ImageView vaisseau = (ImageView) findViewById(R.id.vaisseau);
+        ImageView battleship = (ImageView) findViewById(R.id.vaisseau2);
+        ImageView destroyer = (ImageView) findViewById(R.id.vaisseau3);
+        ImageView submarine = (ImageView) findViewById(R.id.vaisseau4);
+        ImageView patrol = (ImageView) findViewById(R.id.vaisseau5);
+        game.getPlayer1Board().boardView = (BoardView) findViewById(R.id.humanBoardView);
+        game.getPlayer1Board().boardView.setBoard(game.getPlayer1Board());
 
+        // This means the user had already placed boats on the grid but decided to go back to this view and perhaps
+        // change the boats.
+        if (game.getPlayer1Board().grid != null) {
+            game.getPlayer1Board().boardView.coordinatesOfPlayer1Ships = game.getPlayer1Board().grid;
+            game.getPlayer1Board().boardView.invalidate();
+        }
+
+        /* Allow these boat images to be draggable, listen when they are touched */
+        aircraft.setOnTouchListener(new MyTouchListener());
+        battleship.setOnTouchListener(new MyTouchListener());
+        destroyer.setOnTouchListener(new MyTouchListener());
+        submarine.setOnTouchListener(new MyTouchListener());
+        patrol.setOnTouchListener(new MyTouchListener());
+
+        /* Define the particular location where these boat images are allowed to be dragged onto */
+        findViewById(R.id.humanBoardPlacer).setOnDragListener(new MyDragListener());
+        ;
+        Button next = (Button) findViewById(R.id.next); // Advance to the next view
+        Button random = (Button) findViewById(R.id.random); // Place boats at random
+        changeFont(next);
+        changeFont(random);
+        next.setVisibility(View.VISIBLE);
+
+
+    }
 
 }
