029203d5c25f34b7206f17752b8cdf11ca5cef5a
[Rikcore] <[cyprien.chamayou@gmail.com]>
2017-04-27 11:08:47 +0200

    Accueil + choix pseudo

---
+++ app/src/main/java/fr/wcs/bataillenavale/CreateBoard.java
@@ -0,0 +1,35 @@
+package fr.wcs.bataillenavale;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CreateBoard extends AppCompatActivity implements View.OnClickListener {
+
+    TextView textViewCheckpoint;
+    Button buttonReady;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_board);
+
+        textViewCheckpoint = (TextView)findViewById(R.id.textViewCheckpoint);
+        buttonReady = (Button)findViewById(R.id.buttonReady);
+        buttonReady.setOnClickListener(this);
+
+        Intent choiceName = getIntent();
+        String player1Name = choiceName.getStringExtra("player1Name");
+        String player2Name = choiceName.getStringExtra("player2Name");
+
+        textViewCheckpoint.setText(player1Name+" vous allez choisir vos positions.");
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
