19424e290e1dc92349c629efaceb43e904999be5
lebiam <antoine.biamouret@gmail.com>
2017-04-27 23:32:20 +0200

    deck

--- app/src/main/java/fr/wcs/bataillenavale/CreateBoard.java
+++ app/src/main/java/fr/wcs/bataillenavale/CreateBoard.java
@@ -10,11 +10,16 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import static android.view.View.INVISIBLE;
+
 public class CreateBoard extends AppCompatActivity implements View.OnClickListener {
 
     private TextView textViewCheckpoint;
     private Button buttonReady;
+    private Button buttonPush;
     private LinearLayout board;
+    private String player1Name;
+    private String player2Name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,31 +29,41 @@ public class CreateBoard extends AppCompatActivity implements View.OnClickListen
         textViewCheckpoint = (TextView) findViewById(R.id.textViewCheckpoint);
         buttonReady = (Button) findViewById(R.id.buttonReady);
         buttonReady.setOnClickListener(this);
+        buttonPush = (Button)findViewById(R.id.buttonPush);
+        buttonPush.setOnClickListener(this);
+
 
         board = (LinearLayout) findViewById(R.id.board);
 
         Intent choiceName = getIntent();
-        String player1Name = choiceName.getStringExtra("player1Name");
-        String player2Name = choiceName.getStringExtra("player2Name");
+        player1Name = choiceName.getStringExtra("player1Name");
+        player2Name = choiceName.getStringExtra("player2Name");
 
-        textViewCheckpoint.setText(player1Name + " vous allez choisir vos positions.");
+        textViewCheckpoint.setText(player1Name + "vous allez choisir vos positions.");
 
 
 
-        PixelGrid.PixelGridView pixelGrid = new PixelGrid.PixelGridView(this);
+        PixelGridView pixelGrid = new PixelGridView(this);
         pixelGrid.setNumColumns(10);
         pixelGrid.setNumRows(10);
 
         board.addView(pixelGrid);
-        board.setVisibility(View.INVISIBLE);
+        board.setVisibility(INVISIBLE);
     }
+
 
     @Override
     public void onClick(View v) {
 
         if (v == buttonReady) {
             board.setVisibility(View.VISIBLE);
-            buttonReady.setVisibility(View.INVISIBLE);
+            buttonReady.setVisibility(INVISIBLE);
+        }
+        if (v == buttonPush){
+            board.setVisibility(INVISIBLE);
+            textViewCheckpoint.setVisibility(View.VISIBLE);
+            textViewCheckpoint.setText(player2Name+", à ton tour !");
+            buttonReady.setVisibility(View.VISIBLE);
         }
 
     }
---
+++ app/src/main/java/fr/wcs/bataillenavale/PixelGridView.java
@@ -0,0 +1,247 @@
+package fr.wcs.bataillenavale;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by apprenti on 27/04/17.
+ */
+
+    public class PixelGridView extends View {
+        private int numColumns, numRows;
+        private int cellWidth, cellHeight;
+        private Paint blackPaint = new Paint();
+        private boolean[][] cellChecked;
+        boolean [][] mShipTab = new boolean [10][10];
+
+
+
+        private static int CONSTANT_SHIP_FOUR = 0;
+        private static int CONSTANT_SHIP_THREE = 1;
+        private static int CONSTANT_SHIP_THREE_PRIME = 2;
+        private static int CONSTANT_SHIP_TWO = 3;
+        private static int CONSTANT_SHIP_FIVE = 4;
+        private int step = 0;
+
+
+
+        public PixelGridView(Context context) {
+            this(context, null);
+        }
+
+        public PixelGridView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            blackPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        }
+        public boolean[][] getmShipTab() {
+            return mShipTab;
+        }
+
+        public void setNumColumns(int numColumns) {
+            this.numColumns = numColumns;
+            calculateDimensions();
+        }
+
+        public int getNumColumns() {
+            return numColumns;
+        }
+
+        public void setNumRows(int numRows) {
+            this.numRows = numRows;
+            calculateDimensions();
+        }
+
+        public int getNumRows() {
+            return numRows;
+        }
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+            calculateDimensions();
+        }
+
+        private void calculateDimensions() {
+            if (numColumns < 1 || numRows < 1) {
+                return;
+            }
+
+            cellWidth = getWidth() / numColumns;
+            cellHeight = getHeight() / numRows;
+
+            cellChecked = new boolean[numColumns][numRows];
+
+            invalidate();
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawColor(Color.WHITE);
+
+            if (numColumns == 0 || numRows == 0) {
+                return;
+            }
+
+            int width = getWidth();
+            int height = getHeight();
+
+            for (int i = 0; i < numColumns; i++) {
+                for (int j = 0; j < numRows; j++) {
+                    if (cellChecked[i][j]) {
+
+                        canvas.drawRect(i * cellWidth, j * cellHeight,
+                                (i + 1) * cellWidth, (j + 1) * cellHeight,
+                                blackPaint);
+                    }
+                }
+            }
+
+            for (int i = 1; i < numColumns; i++) {
+                canvas.drawLine(i * cellWidth, 0, i * cellWidth, height, blackPaint);
+            }
+
+            for (int i = 1; i < numRows; i++) {
+                canvas.drawLine(0, i * cellHeight, width, i * cellHeight, blackPaint);
+            }
+        }
+
+
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+
+
+            if (step == CONSTANT_SHIP_FOUR ) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    int column = (int) (event.getX() / cellWidth);
+                    int row = (int) (event.getY() / cellHeight);
+
+                    if (column >= 8 || row < 1){
+
+                        return false;
+
+                    } else {
+                        mShipTab[column][row] = true;
+                        mShipTab[column + 1][row] = true;
+                        mShipTab[column + 2][row] = true;
+                        mShipTab[column][row - 1] = true;
+                        cellChecked[column][row] = !cellChecked[column][row];
+                        cellChecked[column + 1][row] = !cellChecked[column + 1][row];
+                        cellChecked[column + 2][row] = !cellChecked[column + 2][row];
+                        cellChecked[column][row - 1] = !cellChecked[column][row - 1];
+                        step ++;
+                        invalidate();
+                    }
+
+                }
+                return true;
+            }
+
+            if (step == CONSTANT_SHIP_THREE) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    int column = (int) (event.getX() / cellWidth);
+                    int row = (int) (event.getY() / cellHeight);
+
+                    if (column >= 8 || mShipTab[column][row] || mShipTab[column + 1][row] || mShipTab[column + 2][row] ) {
+
+                        return false;
+
+                    } else {
+                        mShipTab[column][row] = true;
+                        mShipTab[column + 1][row] = true;
+                        mShipTab[column + 2][row] = true;
+                        cellChecked[column][row] = !cellChecked[column][row];
+                        cellChecked[column + 1][row] = !cellChecked[column + 1][row];
+                        cellChecked[column + 2][row] = !cellChecked[column + 2][row];
+                        step ++;
+                        invalidate();
+                    }
+
+                }
+                return true;
+            }
+            if (step == CONSTANT_SHIP_THREE_PRIME) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    int column = (int) (event.getX() / cellWidth);
+                    int row = (int) (event.getY() / cellHeight);
+
+                    if (column >= 8 || mShipTab[column][row] || mShipTab[column + 1][row] || mShipTab[column + 2][row]  ) {
+
+                        return false;
+
+                    } else {
+                        mShipTab[column][row] = true;
+                        mShipTab[column + 1][row] = true;
+                        mShipTab[column + 2][row] = true;
+                        cellChecked[column][row] = !cellChecked[column][row];
+                        cellChecked[column + 1][row] = !cellChecked[column + 1][row];
+                        cellChecked[column + 2][row] = !cellChecked[column + 2][row];
+                        step ++;
+                        invalidate();
+                    }
+
+                }
+                return true;
+            }
+            if (step == CONSTANT_SHIP_TWO) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    int column = (int) (event.getX() / cellWidth);
+                    int row = (int) (event.getY() / cellHeight);
+
+                    if (column >= 9 || mShipTab[column][row] || mShipTab[column + 1][row] ) {
+                        return false;
+                    } else {
+                        mShipTab[column][row] = true;
+                        mShipTab[column + 1][row] = true;
+                        cellChecked[column][row] = !cellChecked[column][row];
+                        cellChecked[column + 1][row] = !cellChecked[column + 1][row];
+                        step ++;
+                        invalidate();
+                    }
+
+                }
+                return true;
+            }
+            if (step == CONSTANT_SHIP_FIVE) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    int column = (int) (event.getX() / cellWidth);
+                    int row = (int) (event.getY() / cellHeight);
+
+                    if (column >= 8 || row < 1 || mShipTab[column][row] || mShipTab[column + 1][row] || mShipTab[column + 2][row] ||mShipTab[column + 2][row + 1] || mShipTab[column][row - 1]) {
+                        return false;
+                    } else {
+                        mShipTab[column][row] = true;
+                        mShipTab[column + 1][row] = true;
+                        mShipTab[column + 2][row] = true;
+                        mShipTab[column + 2][row + 1] = true;
+                        mShipTab[column][row - 1] = true;
+
+                        cellChecked[column][row] = !cellChecked[column][row];
+                        cellChecked[column + 1][row] = !cellChecked[column + 1][row];
+                        cellChecked[column + 2][row] = !cellChecked[column + 2][row];
+                        cellChecked[column + 2][row - 1] = !cellChecked [column + 2][row - 1];
+                        cellChecked[column][row - 1] = !cellChecked [column][row - 1];
+                        step ++;
+                        invalidate();
+                    }
+
+
+
+
+
+
+                }
+                return true;
+            }
+
+            return true;
+        }
+
+
+    }
+
